2d8e6b5ae90ae280f3df5ec68a680b1984734275
Bruno Resende <rresendebruno@gmail.com>
2022-09-08 01:28:37 -0300

    validações e login

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const app = express();
 // Config JSON response
 app.use(express.json());
 
+// Models
+const User = require("./models/User");
+
 // Open Route - Public Route
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo a API" });
@@ -22,10 +25,91 @@ app.post("/auth/register", async (req, res) => {
 
   // Validations
   if (!name) {
-    res.status(422).json({ msg: "O nome é obrigatório!" });
-    return;
+    return res.status(422).json({ msg: "O nome é obrigatório!" });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "O senha é obrigatório!" });
+  }
+
+  if (password !== confirmpassword) {
+    return res.status(422).json({ msg: "As senhas não conferem!" });
+  }
+
+  // check id user exists
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    return res.status(422).json({ msg: "Por favor utilize outro email!" });
+  }
+
+  // Create password
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hashSync(password, salt);
+
+  // Create user
+  const user = new User({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  try {
+    await user.save();
+    res.status(201).json({ msg: "Usuário criado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Aconteceu um erro no servidor, tente novamente mais tarde!",
+    });
   }
 });
+
+// Login user
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  //validations
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório!" });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória!" });
+  }
+
+  // check if user exists
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
+
+  // check if password match
+  const checkPassword = await bcrypt.compare(password, user.password);
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Senha inválida!" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret
+    );
+    res.status(200).json({ msg: "Autenticação realizada com sucesso", token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      msg: "Aconteceu um erro no servidor, tente novamente mais tarde!",
+    });
+  }
+});
+
 // Credencials
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASS;
